de14e08c7b28bc9bdaf536a9ca75e9d6c541abfb
royartarian <royartarian@gmail.com>
2020-08-06 10:09:46 -0400

    bug fixes about error catch

--- script.js
+++ script.js
@@ -45,13 +45,7 @@ async function getQuote() {
 
     complete();
   } catch (error) {
-    let count = 0;
-    let maxTry = 10;
-    while (count < maxTry) {
-      getQuote();
-      count += 1;
-      console.log(error);
-    }
+        getQuote();
   }
 }
 
